a59641ef6b870c0e504e0666185c7c540cb86c3a
Bakinak <akobbe16@student.aau.dk>
2020-05-06 13:31:12 +0200

    realistic brain alpha wave simulation

--- Embodied Interaction ShamFeedback and EEG/Assets/Scripts/Player.cs
+++ Embodied Interaction ShamFeedback and EEG/Assets/Scripts/Player.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Rigidbody2D myRigidbody;
-    public float moveSpeed;
+    float moveSpeed =3;
 
     public float inputAccuracyBase;
-    float accuracyModifier;
+    public GameObject prepareText, inputText, evilGuy;
+    public float accuracyModifier;
 
     bool jumping;
     bool jumpInitiated;
-    public float jumpForce;
+    float jumpForce = 7;
+
+    bool inputWindow;
+    bool bounce;
+    bool beingSlowed;
 
     //Key Sequence
     bool attemptStarted;
+    bool attemptUsed;
     float sequenceInputTime = 1.5f;
     float timer;
-    float delayTime;
-    float inputDelay;
     private float timePassed;
     float groundHeight;
     //Create key sequence
@@ -37,6 +41,8 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //prepareText.SetActive(false);
+        inputText.SetActive(false);
         animator = GetComponent<Animator>();
         myRigidbody = gameObject.GetComponent<Rigidbody2D>();
         groundHeight = transform.position.y;
@@ -46,6 +52,8 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+
         if (attemptStarted == true)
         {
             timePassed += Time.deltaTime;
@@ -56,11 +64,12 @@ public class Player : MonoBehaviour
         if(transform.position.y > groundHeight)
         {
             jumping = true;
-            
+            moveSpeed = 4.5f;    
         }
         else
         {
             jumping = false;
+            moveSpeed = 3;
         }
         
         if(jumping == false)
@@ -68,15 +77,15 @@ public class Player : MonoBehaviour
             jump();
         }
 
-        if (jumpInitiated)
+        if (jumpInitiated && bounce)
         {
-            delayTime += Time.deltaTime;
-            if(delayTime > inputDelay)
-            {
-                myRigidbody.velocity += new Vector2(0, jumpForce);
-                delayTime = 0;
-                jumpInitiated = false;
-            }
+            myRigidbody.velocity += new Vector2(0, jumpForce);
+            jumpInitiated = false;
+            
+        }
+        if (beingSlowed)
+        {
+            moveSpeed = 1.5f;
         }
 
         animator.SetBool("Jumping", jumping);
@@ -102,30 +111,33 @@ public class Player : MonoBehaviour
 
     bool inputtingSequence()
     {
-
-        if (Input.GetKeyDown(sequence[0])) //If input is the very first key in the sequence, assume user is trying to start sequence from the beginning, and reset timer
+        if (inputWindow && attemptUsed == false)
         {
-            Debug.Log("Sequence Started");
-            timePassed = 0;
-            attemptStarted = true;
-        }
-
-        if (Input.GetKeyDown(sequence[sequenceIndex])) //If the key pressed is the next key we needed to press in the sequence, check that...
-        {
-            sequenceIndex += 1;
-            if (sequenceIndex == sequence.Length) // sequenceIndex is equal to the length of the sequence array, because if it is, we have correctly done the sequence
+            if (Input.GetKeyDown(sequence[0])) //If input is the very first key in the sequence, assume user is trying to start sequence from the beginning, and reset timer
             {
-                resetSequenceAttempt();
-                Debug.Log("Correct Sequence Entered");
-                attemptStarted = false;
-                return true; //return true, so we can do whatever we need to do if we are using either discrete or continuous input.
+                Debug.Log("Sequence Started");
+                timePassed = 0;
+                attemptStarted = true;
             }
-        }
-        else if (Input.anyKeyDown || timePassed > sequenceInputTime) //else, if we input any other key, or spend too long trying to input the sequence, start over.
-        {
-            Debug.Log("failed");
-            resetSequenceAttempt();
-            attemptStarted = false;
+
+            if (Input.GetKeyDown(sequence[sequenceIndex])) //If the key pressed is the next key we needed to press in the sequence, check that...
+            {
+                sequenceIndex += 1;
+                if (sequenceIndex == sequence.Length) // sequenceIndex is equal to the length of the sequence array, because if it is, we have correctly done the sequence
+                {
+                    resetSequenceAttempt();
+                    Debug.Log("Correct Sequence Entered");
+                    attemptStarted = false;
+                    attemptUsed = true;
+                    return true; //return true, so we can do whatever we need to do if we are using either discrete or continuous input.
+                }
+            }
+            else if (Input.anyKeyDown || timePassed > sequenceInputTime) //else, if we input any other key, or spend too long trying to input the sequence, start over.
+            {
+                Debug.Log("failed");
+                resetSequenceAttempt();
+                attemptStarted = false;
+            }
         }
         return false;
     }
@@ -140,11 +152,75 @@ public class Player : MonoBehaviour
     {
         if (Random.value < inputAccuracyBase + accuracyModifier)
         {
-            inputDelay = Random.value;
             return true;
         }
 
         return false;
     }
 
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "inputWindow")
+        {
+            simulatingStress();
+            prepareText.SetActive(false);
+            inputText.SetActive(true);
+            inputWindow = true;
+        }
+
+        if(collision.tag == "bounce")
+        {
+            bounce = true;
+        }
+
+        if (collision.tag == "slow")
+        {
+            beingSlowed = true;
+        }
+
+    }
+
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "inputWindow")
+        {           
+            inputWindow = false;
+        }
+
+        if (collision.tag == "bounce")
+        {
+            bounce = false;
+            attemptUsed = false;
+            prepareText.SetActive(true);
+            inputText.SetActive(false);
+        }
+
+        if (collision.tag == "slow")
+        {
+            beingSlowed = false;
+        }
+    }
+
+    void simulatingStress()
+    {
+        if(evilGuy.transform.position.x - transform.position.x < - 4)
+        {
+            accuracyModifier = 0;
+        }
+        else if(evilGuy.transform.position.x < transform.position.x)
+        {
+            accuracyModifier = Random.Range(4, 12)*2;
+        }
+        else if(evilGuy.transform.position.x - transform.position.x < 4)
+        {
+            accuracyModifier = Random.Range(12, 20)*2;
+        }
+        else
+        {
+            accuracyModifier = Random.Range(20, 30)*2;
+        }
+    }
 }
